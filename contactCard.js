885bf4d35ab2e0c60aa68be8fc903be17591a624
charlesmartinreed <charlesmartinreed@icloud.com>
2020-04-02 17:09:18 -0500

    Create shadow root, template.

--- contactCard.js
+++ contactCard.js
@@ -1,19 +1,35 @@
-// 1. Create the class
+// 1a.
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+        h3 {
+            color: goldenrod;
+        }
+    </style>
+    <div class="contact-card">
+        <h3></h3>
+    </div>
+`;
+
+// 1b. Create the class
 class ContactCard extends HTMLElement {
   constructor() {
     // call the parent class' constructor
     super();
 
     // 2. Create SHADOW DOM to encapsulate styles
-    // this.attachShadow({ mode: "open" });
+    this.attachShadow({ mode: "open" });
+
+    // 3. Attach the template for your reusable element to the shadow root
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     // note that we can access data attributes
-    this.innerHTML = `<h3>This is a Contact Card for ${this.getAttribute(
-      "user_name"
-    )}.</h3>`;
+    this.shadowRoot.querySelector(
+      "h3"
+    ).innerText = `Contact card for ${this.getAttribute("user_name")}`;
   }
 }
 
-// 3. Define the Custom Element
+// 4. Define the Custom Element
 // takes two args: the html tag and the Class to tie that html element to
 window.customElements.define("contact-card", ContactCard);
