50afc5b0f0c3c336fad0cb3f89d59f08137a9cd2
charlesmartinreed <charlesmartinreed@icloud.com>
2020-04-02 17:46:29 -0500

    Demonstrate <slot /> tagging for dynamic info retrieval.

--- contactCard.js
+++ contactCard.js
@@ -1,9 +1,44 @@
-// 1a.
+// 1a. Create the template
+// if you're wondering how we added inner text or dynamic text, see the <slot /> tag
 const template = document.createElement("template");
 template.innerHTML = `
     <style>
+    .contact-card {
+        background: #f4f4f4;
+        width: 35vw;
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+        gap: 1.7rem;
+        margin-bottom: 20px;
+        // border-bottom:  rgba(25, 43, 43, 0.6) 3px solid;
+        padding: 1.7rem;
+        box-shadow: inset 4px 4px 20px rgba(25, 43, 43, 0.2), 2px 2px 8px rgba(25, 43, 43, 0.4);
+        border-radius: 4px;
+    }
+
+    .contact-card img {
+        width: 150px;
+        height: 150px;
+        border-radius: 3px;
+    }
+
+    .contact-card #btn-toggle-info {
+        cursor: pointer;
+        background: dodgerblue;
+        color: whitesmoke;
+        border: 0;
+        border-radius: 5px;
+        padding: 0.5rem 1.2rem;
+    }
+
         h3 {
-            color: goldenrod;
+            color: seagreen;
+            letter-spacing: 0.05rem;
+        }
+
+        p {
+            letter-spacing: 0.08rem;
+            font-size: 12px;
         }
     </style>
     <div class="contact-card">
@@ -11,8 +46,8 @@ template.innerHTML = `
         <div>
             <h3></h3>
             <div class="info">
-                <p>EMAIL</p>
-                <p>PHONE</p>
+                <p><slot name="email"/></p>
+                <p><slot name="phone"/></p>
             </div>
             <button id="btn-toggle-info">Hide Info</button>
         </div>
@@ -32,9 +67,9 @@ class ContactCard extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     // note that we can access data attributes
-    this.shadowRoot.querySelector(
-      "h3"
-    ).innerText = `Contact card for ${this.getAttribute("user_name")}`;
+    this.shadowRoot.querySelector("h3").innerText = `${this.getAttribute(
+      "user_name"
+    )}`;
 
     this.shadowRoot.querySelector("img").src = this.getAttribute("avatar");
   }
